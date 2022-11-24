6045d2941bacdacff34d14620d5b391fb10d735c
Oksana.Maiatina <okmay0625@gmail.com>
2022-11-25 01:09:28 +0300

    + minor adjustments for pages

--- site/main-page/index.js
+++ site/main-page/index.js
@@ -1,14 +1,18 @@
-const module = () => {
-    const inputLogin = document.querySelector('login');
-    const inputPassword = documet.querySelector('pass');
+const inputLogin = document.getElementById('log');
+    const inputPassword = document.getElementById('passWord');
+    const buttonLog = document.getElementById('li');
 
-    inputPassword.addEventListener('input', (event) => {
-        
+    buttonLog.addEventListener('click', () => {
+        if(inputLogin.value ==='' && inputPassword.value === ''){
+            alert("Введите свои данные, пожалуйста ;)");
+        }
+        else if(inputLogin.value === ''){
+            alert("Вы не ввели свой логин, сударь");
+        }
+        else if(inputPassword.value === ''){
+            alert("Вы не ввели свой пароль, сударь");
+        }
+        else{
+            open('/login-page/index.html');
+        }
     })
-
-    inputLogin.addEventListener('input', (event) => {
-        
-    })
-}
-
-export default module;
